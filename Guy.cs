071252e810914ed98049c06ad022b679272cb325
Joanna <aska3001@wp.pl>
2021-07-19 20:55:57 +0200

    Add project files.

---
+++ Guy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joe_and_Bob
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("I don't have enough money to give you $" + amount, Name + " said...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("$" + amount + " is not an amount I am able to receive ", Name + " said...");
+                return 0;
+            }
+        }
+    }
+}
